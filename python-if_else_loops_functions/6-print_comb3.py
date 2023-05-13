2eb4975514d311bd75ee1cd6d6cef677b6dd1f8d
Piperkfb <piperkfb@gmail.com>
2023-05-13 12:37:12 -0700

    there we go

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first in range(10):
     for last in range(first + 1, 10):
         if first == 8 and last == 9:
-            print(f"{first}{last}")
+            print("{:d}{:d}".format(first, last))
         else:
-            print(f"{first}{last}, ", end="")
+            print("{:d}{:d}, ".format(first, last), end="")
