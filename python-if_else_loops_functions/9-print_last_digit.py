629d6c9cd9d7f8d964504b39940e94b673f9f106
Piperkfb <piperkfb@gmail.com>
2023-05-13 13:10:32 -0700

    try again

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+
     if number < 0:
-        digit == -number % 10
+        digit = -number % 10
     else:
-        digit == number % 10
+        digit = number % 10
     return (digit)
