60f1630304c8392df762befb4332a87a81db7538
Piperkfb <piperkfb@gmail.com>
2023-05-13 13:24:02 -0700

    try it plzzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(0, 101):
+        if i == 100:
+            print("Buzz")
+            break
+        if i % 5 == 0 and i % 3 == 0:
+            print("FizzBuzz ")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz ")
+        elif i % 5 != 0 and i % 3 == 0:
+            print("Fizz ")
+        else
+            print(f"{i}")
