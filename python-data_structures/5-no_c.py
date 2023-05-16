f3183f5df080d5dd1b24af9967d05ba5cadb4a45
Piperkfb <piperkfb@gmail.com>
2023-05-16 16:08:06 -0700

    upload mass

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-
-    leng = len(my_string)
-
-    for i in range(leng):
-        
-        if my_string[i] == "c" or my_string[i] == "C":
-            my_string[i] = ""
-
-    return (my_string)
+    my_string = ''.join(char for char in my_string if char not in 'Cc')
+    return my_string
