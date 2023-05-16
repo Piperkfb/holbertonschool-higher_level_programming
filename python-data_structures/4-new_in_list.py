47c74e6a42ac9f3c17073ac6738d2761c2ed75bf
Piperkfb <piperkfb@gmail.com>
2023-05-16 15:41:36 -0700

    diamond so good

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
+    new_list = my_list
+
     if idx < 0:
-        return (list)
+        return (new_list)
+
+    leng = len(my_list)
+
+    if idx > len-1:
+        return (new_list)
+
+    new_list[idx] = element
+
+    return (new_list)
