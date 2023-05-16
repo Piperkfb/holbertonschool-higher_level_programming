f2202783cef64aeded0a97aed620b12fa17329e8
Piperkfb <piperkfb@gmail.com>
2023-05-16 16:01:22 -0700

    try it

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
 def no_c(my_string):
 
+    noc = my_string.copy()
+
+    leng = len(noc)
+
+    for i in range(leng):
+        
+        if noc[i] == "c" or noc[i] == "C":
+            noc[i] = ""
+
+    return (noc)
