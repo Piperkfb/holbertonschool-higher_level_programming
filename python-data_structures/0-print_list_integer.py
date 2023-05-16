1a49b7c4886683c99060ea047b894ccd41625cfc
Piperkfb <piperkfb@gmail.com>
2023-05-16 15:12:56 -0700

    misspell

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def print_lsit_integer(my_list=[]):
+def print_list_integer(my_list=[]):
     leng = len(my_list)
     for i in range(leng):
         print("{:d}".format(my_list[i]))
