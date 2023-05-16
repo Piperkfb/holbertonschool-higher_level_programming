0d9c98e172d7cdeb508a5ac184ed3aee0236398c
Piperkfb <piperkfb@gmail.com>
2023-05-16 15:17:28 -0700

    how about that?

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-def print_lsit_integer(my_list=[]):
+def element_at(my_list, idx):
+    
+    if idx < 0:
+        return (None)
+
     leng = len(my_list)
-    for i in range(leng):
-        print("{:d}".format(my_list[i]))
+
+    if idx > leng:
+        return (None)
+    
+    return (my_list[idx])
