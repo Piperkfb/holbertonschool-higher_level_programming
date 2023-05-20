3b4f7edf6708049dd7b18f163bb7506dbfd70700
Piperkfb <piperkfb@gmail.com>
2023-05-20 10:26:33 -0700

    liekliek

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Area Square """
+
+
+class Square(object):
+    """class variable size"""
+    def __init__(self, size=0):
+        """initialize size"""
+        if size != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """define area"""
+        return self.__size * self.__size
