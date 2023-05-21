3036589d81d19510dd06aa7f327d639a8b8fdfd6
Piperkfb <piperkfb@gmail.com>
2023-05-21 13:02:00 -0700

    push it! :D

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,8 @@
 
 def add_integer(a, b=98):
     """adds two inegers"""
-    if (a is None or a is not isinstance(a, int) and a is not isinstance(a, float)):
+    if (a is None or not isinstance(a, int) and (not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if (b is not isinstance(b, int) and b is not isinstance(b, float)):
+    if (b is None or not isinstance(b, int) and (not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
