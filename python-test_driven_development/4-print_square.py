00a2c8eedded008b2a4d40f8338f9f455489a988
Piperkfb <piperkfb@gmail.com>
2023-05-22 16:09:52 -0700

    curious

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Prints a square: """
+
+
+def print_square(size):
+    """Prints a square"""
+
+    if size < 0:
+        raise TypeError("size must be >= 0")
+
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+
+    for j in range(size):
+        for i in range(size):
+            print("#", end="")
+        print("\n")
