afa1c04fb278d782a9b7ec23e1708d2ce5c5b28c
Piperkfb <piperkfb@gmail.com>
2023-05-22 15:37:23 -0700

    removed messages

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,11 +5,9 @@
 def say_my_name(first_name, last_name=""):
     """Print first and last"""
 
-    #must be a string first and last
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
     elif type(last_name) != str:
         raise TypeError("last_name must be a string")
 
-    #print my name is first last
     print("My name is {:s} {:s}".format(first_name, last_name)
