03b4f041708d39b77de0c234d9094cc11dbfe9da
Piperkfb <piperkfb@gmail.com>
2023-05-22 16:18:03 -0700

    try again

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,11 +5,11 @@
 def print_square(size):
     """Prints a square"""
 
-    if size < 0:
-        raise TypeError("size must be >= 0")
-
     if type(size) != int:
         raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise TypeError("size must be >= 0")
 
     for j in range(size):
         for i in range(size):
