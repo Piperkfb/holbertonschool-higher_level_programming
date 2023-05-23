10ed9e990125969184c47cee6db3255355ac28bc
Piperkfb <piperkfb@gmail.com>
2023-05-23 09:24:48 -0700

    commit?

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""prints adouble new line at points: """
+
+def text_indentation(text):
+    """As seen on TV """
+
+    if type(text) != str:
+        raiseTypeError
+
+    sen = ""
+
+    while i < len(text):
+        if text[i] != "." and text[i] != "?" and text[i] != ":":
+            sen += text[i]
+        else:
+            sen += text [i]
+            print(sen)
+            print("")
+            sen = ""
+            while i < (len(text) - 1) and text[i+1] == "":
+                i += 1
+        i += 1
+    print(sen, end="")
