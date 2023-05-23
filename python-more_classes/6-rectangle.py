b699638f5b3920c5b22b41ad4e8e0b294c49eaa4
Piperkfb <piperkfb@gmail.com>
2023-05-23 10:49:48 -0700

    try that

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle(object):
         """Initialize new class rectangle"""
         self.width = width
         self.height = height
-        Rectangle.number_of_instance += 1
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
