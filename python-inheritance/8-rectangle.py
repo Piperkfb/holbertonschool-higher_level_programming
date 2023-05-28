ff4fab9f7d319ab806ba53e11f5742dc0237bdc9
Piperkfb <piperkfb@gmail.com>
2023-05-28 15:56:48 -0700

    commit this

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """Geometric Base"""
 
+
     def __init__(self, width, height):
         """Initilizing the Rectangle"""
 
