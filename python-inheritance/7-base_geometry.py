c2d6db4d49554789207cbf4716d4ffe01254f03d
Piperkfb <piperkfb@gmail.com>
2023-05-28 12:17:10 -0700

    update

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""You think I'm not documenting?"""
+
+
+class BaseGeometry(object):
+    """Empty Students"""
+
+    def area(self):
+        """return area"""
+        raise Exception("area() is not implemented")
