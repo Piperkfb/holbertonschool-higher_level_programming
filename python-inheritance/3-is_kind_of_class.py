9c35f37c3c0e8f12fb12e46e2fcfffb74c1543be
Piperkfb <piperkfb@gmail.com>
2023-05-28 11:57:14 -0700

    try

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 """Road gets rocky"""
 
+
+def is_kind_of_class(obj, a_class):
+    """Returns instance comparison"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
