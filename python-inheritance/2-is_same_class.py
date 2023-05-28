b608b24bbdf9632e5545be02417192a4f94a5926
Piperkfb <piperkfb@gmail.com>
2023-05-28 11:29:08 -0700

    blep

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -5,7 +5,7 @@
 def is_same_class(obj, a_class):
     """Returns True is object is instance of class"""
 
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     else:
         return False
