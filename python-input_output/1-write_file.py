9cdf7e1eac228646c807e4b845c33f369b2bf16a
Piperkfb <piperkfb@gmail.com>
2023-05-28 19:24:27 -0700

    try that

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A stringy sreing"""
+
+
+def write_file(filename="", text=""):
+    """Whacha wanna say?"""
+    with open(filename, mode="w", encoding="utf-8" as Filez
+            Filez.write(text)
