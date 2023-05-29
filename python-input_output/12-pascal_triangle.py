8c0ab3cd615a7b2def858e381274909829486114
Piperkfb <piperkfb@gmail.com>
2023-05-29 10:27:58 -0700

    done?

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal is a disney character"""
+
+
+def pascal_triangle(n):
+    """Their triangle"""
+
+    if n <=0:
+        return ""
+
+    triangle = [[1]]
+    for cur_row in range(1, n):
+        row = [1]
+        prev_row = triangle[cur_row - 1]
+        for wlem in range(1, cur_row):
+            row.append(prev_row[elem] + prev_row[elem - 1])
+        row.append(1)
+        triangle.append(row)
+    return (triangle)
