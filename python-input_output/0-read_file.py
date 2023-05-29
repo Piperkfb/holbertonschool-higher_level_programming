34240d67c76d0d9677d1f19b0bdb33a2476129a8
Piperkfb <piperkfb@gmail.com>
2023-05-28 19:15:16 -0700

    no parathesis

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 def read_file(filename=""):
     """What does this say"""
-    with open("filename", encoding="utf-8") as DaFile:
+    with open(filename, encoding="utf-8") as DaFile:
 
         print(DaFile.read())
