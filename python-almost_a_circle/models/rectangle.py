7f7c34f5590a5af5a3499d57e516738bff118945
Piperkfb <piperkfb@gmail.com>
2023-06-01 10:32:40 -0700

    try

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -103,3 +103,7 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print("")
+    
+    def __str__(self):
+        """Displays attributes in a nice way"""
+        return ('[Rectangle] ({}) {}/{} - {}/{}'.format(self.id, self.x, self.y, self.width, self.height)
