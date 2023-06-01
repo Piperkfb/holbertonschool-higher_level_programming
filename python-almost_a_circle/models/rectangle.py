d804ada4bc8d4f2d291977a643a378144af7a45c
Piperkfb <piperkfb@gmail.com>
2023-06-01 10:27:25 -0700

    oops

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,9 @@ class Rectangle(Base):
     def area(self):
         """The area of the rectangle"""
         return self.width * self.height
+
+    def display(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print("#", end="")
+            print("")
