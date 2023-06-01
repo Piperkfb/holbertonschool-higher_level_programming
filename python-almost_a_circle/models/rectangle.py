0662897bc831c8c69f8cf5d3882346a9d02eaf87
Piperkfb <piperkfb@gmail.com>
2023-06-01 09:35:11 -0700

    push?

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,4 +8,11 @@ from models.base import Base
 class Rectangle(Base):
     """Welcome to Rekt class"""
 
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """whut"""
+        self.__width = width
+        self.__height = heigh
+        self.__x = x
+        self.__y = y
 
+        super().__init__(id)
