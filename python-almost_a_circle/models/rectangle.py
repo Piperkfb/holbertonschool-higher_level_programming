b2d123ccb05140902f5a2f789e5daf3162719850
Piperkfb <piperkfb@gmail.com>
2023-06-01 09:40:47 -0700

    ?

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,10 +8,8 @@ from models.base import Base
 class Rectangle(Base):
     """Welcome to Rekt class"""
 
-    self.__width = width
-    self.__height = height
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """whut"""
-
+        self.width = width
+        self.height = height
         super().__init__(id)
