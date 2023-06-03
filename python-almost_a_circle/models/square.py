2f25ddb931f5fbb4f895badd835513d68d6db481
Piperkfb <piperkfb@gmail.com>
2023-06-03 11:57:47 -0700

    try that

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -14,46 +14,15 @@ class Square(Rectangle):
     @property
     def size(self):
         """getter for size"""
-        return self.__size
+        return self.width
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """setter for size"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be > 0")
-        self.__size = size
-
-    @property
-    def x(self):
-        """Getter for X"""
-        return self.__x
-
-    @x.setter
-    def x(self, x):
-        """Setter for X"""
-        if type(x) != int:
-            raise TypeError("x must be an integer")
-        if x < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = x
-
-    @property
-    def y(self):
-        """Getter for Y"""
-        return self.__y
-
-    @y.setter(self, y):
-        """Setter for Y"""
-        if type(y) != int:
-            raise TypeError("y must be an integer")
-        if y < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = y
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Display the Square"""
-        return ('[Square] ({}) {}/{} - {}/{}'.format(self.id, self.x, 
-                                                        self.y, self.size,
-                                                        self.size))
+        return ('[Square] ({}) {}/{} - {}'.format(self.id, self.x, 
+                                                        self.y, self.width))
