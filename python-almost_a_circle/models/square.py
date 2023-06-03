c96b596345c9951fcd82ba9489ad28172da3a7d1
Piperkfb <piperkfb@gmail.com>
2023-06-03 12:04:16 -0700

    hora?

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -26,4 +26,37 @@ class Square(Rectangle):
     def __str__(self):
         """Display the Square"""
         return ('[Square] ({}) {}/{} - {}'.format(self.id, self.x,
-                                                self.y, self.width))
+                                                  self.y, self.width))
+    def update(self, *args, **kwargs):
+        """Update what you want"""
+        if args and args != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        super().__init__(self.width,
+                                            self.height, self.x,
+                                            self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+        elif kwargs and len(kwargs) != 0:
+            for i, j in kwargs.items():
+                if i == "id":
+                    if i is None:
+                        super().__init__(self.width, self.height,
+                                            self.x, self.y)
+                    else:
+                        self.id = j
+                elif i == "size":
+                    self.size = j
+                elif i == "x":
+                    self.x = j
+                elif i == "y":
+                    self.y = j
