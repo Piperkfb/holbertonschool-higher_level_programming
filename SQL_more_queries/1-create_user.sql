6ab2099faa434b387b60de05711223db2244d16a
Piperkfb <piperkfb@gmail.com>
2023-06-25 11:58:11 -0700

    hmm

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- creates a server user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
+IDENTIFIED BY 'user_od_1.pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+IDENTIFIED BY 'user_0d_1pwd';
+FLUSH PRIVILEGES;
