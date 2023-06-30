b3bcd6744d20cf102c8f7ee34d3c96606ca3f0dd
Piperkfb <piperkfb@gmail.com>
2023-06-30 18:43:22 +0000

    work?

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,9 @@
 -- creates a server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_od_1.pwd';
-GRANT ALL PRIVILEGES ON *.*
-TO 'user_0d_1'@'localhost';
-IDENTIFIED BY 'user_0d_1pwd';
+CREATE USER 
+    IF NOT EXISTS 'user_0d_1'@'localhost' 
+    IDENTIFIED BY 'user_od_1.pwd';
+GRANT ALL PRIVILEGES 
+    ON *.*
+    TO 'user_0d_1'@'localhost';
+    IDENTIFIED BY 'user_0d_1pwd';
 FLUSH PRIVILEGES;
