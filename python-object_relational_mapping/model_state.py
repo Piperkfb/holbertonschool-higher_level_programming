480cf49441532730e2ba0bf915ca9294af978799
Piperkfb <piperkfb@gmail.com>
2023-07-03 19:52:11 -0700

    try that

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -17,3 +17,4 @@ class State(Base):
     id = Column(Integer, primary_key=True, nullable=False,
                 unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
+    
