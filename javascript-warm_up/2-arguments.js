7c471189bef4d6874bd96f65c506be3985911caf
Piperkfb <piperkfb@gmail.com>
2023-07-30 14:28:11 -0700

    Q 2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,15 +2,12 @@
 
 const count = process.argv.length;
 switch (count) {
-    
-    case 2:
+  case 2:
     console.log('No argument');
     break;
-
-    case 3:
+  case 3:
     console.log('Argument found');
     break;
-
-    default:
+  default:
     console.log('Arguments found');
 }
