0617b2b8dd750027887d43298c8eaa520e1acc28
Piperkfb <piperkfb@gmail.com>
2023-08-05 12:21:04 -0700

    Q3

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const movID = process.argv[2];
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + movID;
+
+request.get(url, function(err, data, body) {
+    if (err) {
+        throw err;
+    } else {
+      const jsonData = JSON.parse(body);
+      const name = jsonData.name;
+      console.log(name);
+    }
+});
