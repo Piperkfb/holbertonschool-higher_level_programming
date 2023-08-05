1bd7e9316a4ecbe66d02ce44f43d95d4fea6bad9
Piperkfb <piperkfb@gmail.com>
2023-08-05 12:47:51 -0700

    try this way first Q5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const path = process.argv[3];
+const fs = require('fs');
+
+request.get(url, 'utf8', function(err, data, body) {
+    if (err) throw err;
+    const Jason = JSON.parse(body);
+    fs.writeFile(path, Jason, function(err2) {
+        if (err2) throw err2;
+    });
+});
