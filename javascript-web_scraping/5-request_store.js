2bf8b29a0196238a23f2d7180e2d78915f3edb1e
Piperkfb <piperkfb@gmail.com>
2023-08-05 12:56:35 -0700

    little change

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -5,9 +5,8 @@ const path = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
-request.get(url, function(err, data) {
-    if (err) throw err;
-    fs.writeFile(path, data, 'utf8', function(err2) {
+request.get(url, function(err, data, body) {
+    fs.writeFile(path, body, 'utf8', function(err2) {
         if (err2) throw err2;
     });
 });
